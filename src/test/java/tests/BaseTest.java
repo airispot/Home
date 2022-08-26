7521c223e66d343cba7d95631e242579f98abed8
airispot <airispot@gmail.com>
2022-08-26 19:56:08 +0300

    Added new tests

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,67 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.*;
+import pages.*;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    protected LoginPage loginPage;
+    protected ProductPage productPage;
+
+    protected CartPage cartPage;
+
+    protected CheckoutCompletePage checkoutCompletePage;
+
+    protected CheckoutStepOnePage checkoutStepOnePage;
+
+    protected CheckoutStepTwoPage checkoutStepTwoPage;
+
+    @Parameters({"browser"})
+    @BeforeClass(alwaysRun = true)
+    public void setUp(@Optional("chrome") String browser) {
+
+        String browserName = System.getProperty("browser");
+        if(browserName.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--start-maximized");
+            driver = new ChromeDriver(options);
+        } else if (browserName.equals("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else {
+            throw new Error("Only chrome and firefox are supported at the moment");
+        }
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        loginPage = new LoginPage(driver);
+        productPage = new ProductPage(driver);
+        cartPage = new CartPage(driver);
+        checkoutCompletePage = new CheckoutCompletePage(driver);
+        checkoutStepOnePage = new CheckoutStepOnePage(driver);
+        checkoutStepTwoPage = new CheckoutStepTwoPage(driver);
+    }
+
+    @BeforeMethod (alwaysRun = true)
+    public void navigate() {
+        loginPage.open();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+    }
+}
